12a83b64420bd71a0b6a9c73a8fed7b1d8fd2ce7
ewen <burban_ewen@mailfence.com>
2022-05-06 10:45:57 +0200

    update 1.559 remove sudo in cluster_configure

--- cluster_configure.sh
+++ cluster_configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo singularity build --remote container/bcftools.sif container/bcftools.def 
-sudo singularity build --remote container/python.sif container/python.def 
-sudo singularity build --remote container/shapeit.sif container/shapeit.def 
-sudo singularity build --remote container/R.sif container/R.def 
+singularity build --remote container/bcftools.sif container/bcftools.def 
+singularity build --remote container/python.sif container/python.def 
+singularity build --remote container/shapeit.sif container/shapeit.def 
+singularity build --remote container/R.sif container/R.def 
